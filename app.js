380555672a1b5b7647726677d72f0f9e444a0889
John McCullough <john.mccullough@navomi.com>
2021-08-25 09:23:07 -0400

    Init Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const usersRouter = require('./routes/users');
+const articlesRouter = require('./routes/articles');
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/service/users', usersRouter);
+app.use('/service/articles', articlesRouter);
+
+module.exports = app;
